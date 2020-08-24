35c5be66cb37078ba873742371553d41d4738584
hilbug <65197724+hilbug@users.noreply.github.com>
2020-08-24 12:58:13 -0400

    add prompts for role, employee, and manager

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+// require project modules
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const teamQuestions = require("./lib/teamQuestions");
+const { roleQuestions, employeeQuestions, managerQuestions, engineerQuestions, internQuestions } = require("./lib/teamQuestions");
+
+// npm packages
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -10,7 +15,93 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+// initialize empty array to store employees
+let employees = [];
+/*
+// Prompst user with questions about employees
+let promptUser = () => {
+    let employee = {};
+    return inquirer.prompt('roleQuestions').then((answer) => {
+        employee['empRole'] = answer.empRole;
+        inquirer.prompt(employeeQuestions).then((answer) => {
+            employee['empName'] = answer.empName;
+            employee['empID'] = answer.empID;
+            employee['empEmail'] = answer.empEmail;
+            if (employee['empRole'] === 'Manager') {
+                inquirer.prompt(managerQuestions).then((answer) => {
+                    employee['mgrOfficeNumber'] = answer.mgrOfficeNumber;
+                    startQuestions();
+                });
+            }
+            else if (employee['empRole'] === 'Done') {
+                console.log('Done with team members!');
+                console.log(employee);
+            }
+        });
+    });
+}*/
 
+let promptUser = () => {
+    // name, id, email, custom
+    let employee = {};
+    // first ask for a role
+    return inquirer.prompt(roleQuestions).then((answer) => {
+        // save role to employee object
+        employee['empRole'] = answer.empRole;
+        console.log(answer.empRole);
+        // then ask general employee questions
+        inquirer.prompt(employeeQuestions).then((answer) => {
+            // save general answers to employee object
+            employee['empName'] = answer.empName;
+            employee['empId'] = answer.empId;
+            employee['empEmail'] = answer.empEmail;
+            // if the role was manager, ask manager specific question
+            if (employee['empRole'] === 'Manager') {
+                inquirer.prompt(managerQuestions).then((answer) => {
+                    // save manager answer to employee object
+                    employee['mgrOfficeNumber'] = answer.mgrOfficeNumber;
+                    console.log(employee);
+                    // create new Manager construct
+                    const addEmployee = new Manager(employee['empName'],employee['empId'],employee['empEmail'],employee['mgrOfficeNumber']);
+                    // add Manager construct to employees array
+                    employees.push(addEmployee);
+                    console.log(employees);
+                });
+            // if the role was engineer, ask engineer specific question
+            } else if (employee['empRole'] === 'Engineer') {
+                inquirer.prompt(engineerQuestions).then((answer) => {
+                    // save engineer answer to employee object
+                    employee['engGithub'] = answer.engGithub;
+                    console.log(employee);
+                });
+            // if the role was intern, ask intern specific question
+            } else if (employee['empRole'] === 'Intern') {
+                inquirer.prompt(internQuestions).then((answer) => {
+                    // save intern answer to employee object
+                    employee['internSchool'] = answer.internSchool;
+                    console.log(employee);
+                });
+            }
+        });
+    });
+}
+
+let init = async () => {
+    console.log("Welcome to the Team HTML Page Generator! You will be guided through a series of questions to create profiles for your employees. If you don't have an answer right now, you can leave it blank. At the end, you will have a team.html file in the Output folder.");
+    try {
+        // Get user answers
+        const answers = await promptUser();
+        console.log(answers);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+init();
+
+
+
+// NOTES from assignment......
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
